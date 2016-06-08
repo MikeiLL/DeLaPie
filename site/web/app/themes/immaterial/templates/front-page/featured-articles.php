49260e58fedb615af1328c0828c18f5a78acaf6d
Mike iLL Kilmer <mike@mzoo.org>
2016-06-08 15:55:23 -0500

    Use specific excerpt_more for articles cards.

--- site/web/app/themes/immaterial/templates/front-page/featured-articles.php
+++ site/web/app/themes/immaterial/templates/front-page/featured-articles.php
@@ -1,3 +1,20 @@
+<?php
+
+namespace Roots\Sage\featured_articles;
+
+/**
+ * Clean up the_excerpt()
+ */
+function excerpt_more_card() {
+    return '&hellip; <div class="mdl-card__actions mdl-card--border">' .
+          '<div class="mdl-layout-spacer"></div>' .
+            '<a href="' . get_permalink() . '" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Read More</a>' .
+          '</div>';
+}
+add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more_card');
+
+?>
+
 				<?php
 
 						$args = array(
@@ -5,7 +22,7 @@
 			'meta_key' => '_featured-post',
 			'meta_value' => 1
 			);
-			$featured = new WP_Query($args);
+			$featured = new \WP_Query($args);
 
 			if ($featured->have_posts()): while($featured->have_posts()): $featured->the_post(); ?>
 			<div class="mdl-card mdl-cell mdl-cell--12-col mdl-cell--8-col-tablet mdl-shadow--1dp">
