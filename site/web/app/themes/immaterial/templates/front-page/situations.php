7e676e00c3f8f05f33119cb3ebf523830fec096a
Mike iLL Kilmer <mike@mzoo.org>
2016-06-08 14:34:54 -0500

    Move this off of the front page for cleaner page.

---
+++ site/web/app/themes/immaterial/templates/front-page/situations.php
@@ -0,0 +1,47 @@
+<?php
+
+$contact_our_office = '<p>Please contact my office for a free case evaluation. </p>';
+
+?>
+			<div class="mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone">
+
+				<ul>
+            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="one" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Falls </li>
+
+            <div class="response-definition one">
+                <p>Falls are one of the most common injuries in long-term care facilities. According to the CDC, between 50% and 75% of nursing home residents fall each year.  That’s twice the rate of falls among older adults living in the community.  Residents often fall more than once and about 35% of fall injuries occur among residents who cannot walk.  About 10% to 20% of nursing home falls cause serious injuries; 2% to 6% cause fractures.  The law requires facilities to implement and maintain fall prevention measures to protect residents, but facilities must be proactive in order to do so successfully.  The CDC reports that restraints do not help prevent falls.  Muscle weakness, gait problems, wet floors, poor lighting, incorrect bed height, improperly fitted wheelchairs, certain medicines, poor foot care, poorly fitting shoes, improper or incorrect use of walking aids, inattentive staff and low staff-to-resident ratios increase risk of falls in elders.</p>
+								<?= $contact_our_office ?>
+
+            </div>
+             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="two" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Bedsores</li>
+
+            <div class="response-definition two">
+                <p>Bedsores (also known as decubitus ulcers, pressure ulcers or pressure sores) are one of the most common injuries in long-term care facilities.  According to the CDC, approximately 1 in 10 nursing home residents had a bedsore.  They develop when a resident is left in one position for too long and commonly form where bones are close to skin, such as the heel, ankle, tailbone, hip, shoulder, back, elbow, and back of the head.  Left unchecked serious infection can develop resulting in amputation, sepsis or death.  The law requires facilities to be proactive in preventing bedsores by keeping skin clean and dry, changing position and using pillows and products that relieve pressure, for example.  Thus, if your loved one is developing bed sores, it may be a sign that the nursing staff is neglecting its duties or not providing the appropriate level of care. </p>
+                <?= $contact_our_office ?>
+            </div>
+             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="three" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Situation Number Three</li>
+
+            <div class="response-definition three">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
+            </div>
+      </ul>
+    </div>
+    <div class="mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone">
+      			<ul>
+              <li class="list__item" data-gk-area-state="closed" data-gk-open-class="four" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Situation Number Four</li>
+
+            <div class="response-definition four">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
+            </div>
+             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="five" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Situation Number Five</li>
+
+            <div class="response-definition five">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
+            </div>
+             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="six" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Situation Number Six</li>
+
+            <div class="response-definition six">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
+            </div>
+      	</ul>
+    	</div>
