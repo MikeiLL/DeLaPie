7f78b39db7feba1be1c078d5a15993fb09ad096b
Mike iLL Kilmer <mike@mzoo.org>
2016-06-22 21:32:33 -0500

    Display "situations" dynamically from assoc array.

--- site/web/app/themes/immaterial/templates/front-page/situations.php
+++ site/web/app/themes/immaterial/templates/front-page/situations.php
@@ -1,118 +1,34 @@
 <?php
 
 $contact_our_office = '<p>Please contact my office for a free case evaluation. </p>';
+$situations = array(
+"Falls"=>"Falls are one of the most common injuries in long-term care facilities. According to the <abbr>CDC</abbr>, between 50% and 75% of nursing home residents fall each year.  That’s twice the rate of falls among older adults living in the community.  Residents often fall more than once and about 35% of fall injuries occur among residents who cannot walk.  About 10% to 20% of nursing home falls cause serious injuries; 2% to 6% cause fractures.  The law requires facilities to implement and maintain fall prevention measures to protect residents, but facilities must be proactive in order to do so successfully.  The <abbr>CDC</abbr> reports that restraints do not help prevent falls.  Muscle weakness, gait problems, wet floors, poor lighting, incorrect bed height, improperly fitted wheelchairs, certain medicines, poor foot care, poorly fitting shoes, improper or incorrect use of walking aids, inattentive staff and low staff-to-resident ratios increase risk of falls in elders.",
+"Bedsores"=>"Bedsores (also known as decubitus ulcers, pressure ulcers or pressure sores) are one of the most common injuries in long-term care facilities.  According to the <abbr>CDC</abbr>, approximately 1 in 10 nursing home residents had a bedsore.  They develop when a resident is left in one position for too long and commonly form where bones are close to skin, such as the heel, ankle, tailbone, hip, shoulder, back, elbow, and back of the head.  Left unchecked serious infection can develop resulting in amputation, sepsis or death.  The law requires facilities to be proactive in preventing bedsores by keeping skin clean and dry, changing position and using pillows and products that relieve pressure, for example.  Thus, if your loved one is developing bed sores, it may be a sign that the nursing staff is neglecting its duties or not providing the appropriate level of care.",
+"Wrongful Death"=>"",
+"Dehydration & Malnutrition"=>"",
+"False Imprisonment"=>"",
+"Poor Hygiene"=>"",
+"Sepsis"=>"",
+"Fractures"=>"",
+"Sexual Abuse"=>"",
+"Medication Errors"=>"",
+"Unexplained Injuries"=>""
 
+ );
 ?>
+      <ul class="items definitions">
+<?php $count = 0;
+foreach($situations as $situation => $definition):
+  $count++;
+  if ($definition == "")
+    $definition = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat.";
+  ?>
+         <li class="list__item" data-gk-area-state="closed" data-gk-open-class="<?= $count ?>" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active"><?= $situation ?> </li>
 
-				<ul class="items">
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="one" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Falls </li>
-
-            <div class="response-definition one">
-                <p>Falls are one of the most common injuries in long-term care facilities. According to the <abbr>CDC</abbr>, between 50% and 75% of nursing home residents fall each year.  That’s twice the rate of falls among older adults living in the community.  Residents often fall more than once and about 35% of fall injuries occur among residents who cannot walk.  About 10% to 20% of nursing home falls cause serious injuries; 2% to 6% cause fractures.  The law requires facilities to implement and maintain fall prevention measures to protect residents, but facilities must be proactive in order to do so successfully.  The <abbr>CDC</abbr> reports that restraints do not help prevent falls.  Muscle weakness, gait problems, wet floors, poor lighting, incorrect bed height, improperly fitted wheelchairs, certain medicines, poor foot care, poorly fitting shoes, improper or incorrect use of walking aids, inattentive staff and low staff-to-resident ratios increase risk of falls in elders.</p>
+            <div class="response-definition <?= $count ?>">
+                <p><?= $definition ?></p>
 								<?= $contact_our_office ?>
 
             </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="two" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Bedsores</li>
-
-            <div class="response-definition two">
-                <p>Bedsores (also known as decubitus ulcers, pressure ulcers or pressure sores) are one of the most common injuries in long-term care facilities.  According to the <abbr>CDC</abbr>, approximately 1 in 10 nursing home residents had a bedsore.  They develop when a resident is left in one position for too long and commonly form where bones are close to skin, such as the heel, ankle, tailbone, hip, shoulder, back, elbow, and back of the head.  Left unchecked serious infection can develop resulting in amputation, sepsis or death.  The law requires facilities to be proactive in preventing bedsores by keeping skin clean and dry, changing position and using pillows and products that relieve pressure, for example.  Thus, if your loved one is developing bed sores, it may be a sign that the nursing staff is neglecting its duties or not providing the appropriate level of care. </p>
-                <?= $contact_our_office ?>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="three" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Wrongful Death</li>
-
-            <div class="response-definition three">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="eight" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Dehydration & Malnutrition</li>
-
-            <div class="response-definition eight">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="nine" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">False Imprisonment</li>
-
-            <div class="response-definition nine">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="ten" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Poor Hygiene</li>
-
-            <div class="response-definition ten">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="four" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Sepsis</li>
-
-            <div class="response-definition four">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="five" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Fractures</li>
-
-            <div class="response-definition five">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="six" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Sexual Abuse</li>
-
-            <div class="response-definition six">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="seven" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Medication Errors</li>
-
-            <div class="response-definition seven">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="eleven" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Unexplained Injuries</li>
-
-            <div class="response-definition eleven">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-             <li class="list__item" data-gk-area-state="closed" data-gk-open-class="twelve" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Billing Fraud</li>
-
-            <div class="response-definition twelve">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="four" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Sepsis</li>
-
-            <div class="response-definition four">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="five" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Fractures</li>
-
-            <div class="response-definition five">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="six" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Sexual Abuse</li>
-
-            <div class="response-definition six">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="seven" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Medication Errors</li>
-
-            <div class="response-definition seven">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="eleven" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Unexplained Injuries</li>
-
-            <div class="response-definition eleven">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
-            <li class="list__item" data-gk-area-state="closed" data-gk-open-class="twelve" data-gk-open-span-glyph="closed" data-gk-close-span-glyph="active">Billing Fraud</li>
-
-            <div class="response-definition twelve">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna urna, pretium eu pretium in, dictum nec nisl. Proin vehicula convallis mi, in vestibulum magna laoreet eget. Duis quis dignissim nibh, at aliquet elit. Mauris ultrices a sapien quis hendrerit. Pellentesque in nisi dui. Pellentesque commodo diam sapien, eget egestas elit rutrum sodales. Etiam sapien nisi, pretium eu consequat ut, sollicitudin vitae erat. </p>
-            </div>
-
+  <?php endforeach; ?>
       	</ul>
