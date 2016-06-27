e2ebce516423b182b9ba49857deaf92b95b3fa85
Mike iLL Kilmer <mike@mzoo.org>
2016-06-27 16:25:41 -0500

    Move inline script out of illegal location.

--- site/web/app/themes/immaterial/base.php
+++ site/web/app/themes/immaterial/base.php
@@ -12,7 +12,51 @@ use Roots\Sage\Assets;
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
   <?php echo file_get_contents(Assets\asset_path('images/sprites/sprite-defs.svg')); ?>
+<script type="text/javascript">
 
+// SVG feature detection
+;(function (window, document, undefined) {
+
+	'use strict';
+
+	// SVG feature detection
+	var supports = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
+	if ( !supports ) return;
+
+	// Add `.svg` class to <html> element
+	document.documentElement.className += (document.documentElement.className ? ' ' : '') + 'svg';
+
+})(window, document);
+
+//https://github.com/iamdustan/smoothscroll
+document.addEventListener('DOMContentLoaded', function(e) { setTimeout(function() {
+
+// Make header disappear when scrolling down and return when scroll up.
+  var main_header = document.getElementsByClassName('mdl-layout__header')[0];
+      main_container = document.getElementsByClassName('mdl-layout__content')[0];
+      header_rows = document.getElementsByClassName("mdl-layout__header-row");
+      fade_class = "mdl-layout__header-row fade_out";
+      opaque_class = "mdl-layout__header mdl-color--grey-800 mdl-color-text--yellow-500 mdl-layout__header--waterfall";
+      transparent_class = "mdl-layout__header mdl-layout__header--transparent";
+      display_class = "mdl-layout__header-row";
+      lastVal = 0;
+
+  }, 2000) // /setTimeOut
+}, false); // /addEventListener
+
+function modifyMainHeader(visibility_class, opacity_class, hide) {
+  for (var i = 1; i < header_rows.length; i++) {
+    header_rows[i].className = visibility_class;
+  }
+  main_header.className = opacity_class;
+  if (hide === true) {
+    main_header.setAttribute("style", "min-height:16px;transition-duration: 0.8s;transition-property: min-height, box-shadow;");
+  } else {
+    main_header.removeAttribute("style");
+  }
+}
+
+</script>
     <!--[if lt IE 9]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'immaterial'); ?>
