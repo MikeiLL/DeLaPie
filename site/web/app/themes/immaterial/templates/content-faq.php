ba3b3b63164a97fbd748730a8a1ba06cc3d2f809
Mike iLL Kilmer <mike@mzoo.org>
2016-07-07 12:55:02 -0500

    Mostly updating FAQ rendering.

--- site/web/app/themes/immaterial/templates/content-faq.php
+++ site/web/app/themes/immaterial/templates/content-faq.php
@@ -1,13 +1,19 @@
   <article <?php post_class(); ?>>
-    <header>
-      <h2 class="entry-title"><?php the_title(); ?></h2>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
+       <?php $item_name = str_replace(array(' ', '?'), array('-', ''), get_the_title()); ?>
+      <ul class="questions">
+
+         <li class="list__item">
+        <button class="btn active list-def" data-gk-close-span-glyph="active" data-gk-open-span-glyph="closed" data-gk-open-class="<?php echo $item_name ?>" data-gk-area-state="closed" title="Click to Open"><?= the_title(); ?></button>
+
+            <div class="response-definition <?php echo $item_name ?>">
+              <?php the_content(); ?>
+            </div>
+          </li>
+      	</ul>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'immaterial'), 'after' => '</p></nav>']); ?>
     </footer>
     <?php //comments_template('/templates/comments.php'); ?>
   </article>
+
+
