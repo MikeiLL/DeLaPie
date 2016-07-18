e22189353395675304f8a0bcf668b013792a043a
Mike iLL Kilmer <mike@mzoo.org>
2016-07-18 18:48:02 -0500

    Blockquotes on front page.

--- site/web/app/themes/immaterial/front-page.php
+++ site/web/app/themes/immaterial/front-page.php
@@ -34,7 +34,7 @@ use Roots\Sage\Assets;
   <div id="situations" class="content-grid mdl-grid mdl-color--grey-500 mdl-color-text--alabaster-50 front-page__cover">
 
     <h2>Nursing Home Abuse and Neglect Cases</h2>
-    <h3>Not just what I do, <br class="mdl-cell--phone-only" />it's <em>all</em> I do.</h3>
+    <blockquote>Not just what I do, <br class="mdl-cell--phone-only" />it's <em>all</em> I do.</blockquote>
     <?php get_template_part('templates/front-page/situations'); ?>
 
   </div><!-- ./#situations -->
